766c0d2c5d05bc7f1b38e42f2e634b3a89635ae8
IsaacMother <andrey.turbov@gmail.com>
2017-06-11 01:02:34 +0300

    updated

--- profile.d/system-wide-settings.sh
+++ profile.d/system-wide-settings.sh
@@ -4,6 +4,6 @@
 
 alias du0='du -hs'
 alias du1='du -h --max-depth=1'
-alias ll='ls -FlAh --color=auto --group-directories-first --indicator-style=file-type'
+alias ll='ls -FGlAh --color=auto --group-directories-first --indicator-style=file-type'
 alias ls='ls --color=auto'
 alias top='htop'
