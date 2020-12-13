2dfb7cbb0a05820dda09dfecb03fbbab3b912dd0
unsip <unsip@protonmail.com>
2020-12-13 23:10:23 +0300

    Migration to yadm-compatible layout.

--- profile.d/system-wide-settings.sh
+++
@@ -1,9 +0,0 @@
-#
-# System-wide aliases
-#
-
-alias du0='du -hs'
-alias du1='du -h --max-depth=1'
-alias ll='ls -FGlAh --color=auto --group-directories-first --indicator-style=file-type'
-alias ls='ls --color=auto'
-alias top='htop'
